507a01e181c2f9911677eb9e0156d3ca4a19cc33
Gaje Robarge <gajer@paulmitchell.edu>
2020-01-27 10:30:14 -0700

    finished backend and set up requests for all players and player stats

---
+++ dummyDatabase.js
@@ -0,0 +1,7 @@
+let players = [
+    { _id: 1, name: "Gaje", runs: "1789" },
+    { _id: 2, name: "Jayden", runs: "666" },
+    { _id: 3, name: "Jerry", runs: "3000" },
+    { _id: 4, name: "Bobby", runs: "1243" },
+]
+module.exports = players;
